55545155eeb8bf6a9130604a8221df73b9755e81
ABastinou <101596380+BASTINOU777@users.noreply.github.com>
2023-02-17 15:00:23 +0100

    add skybox 🌞

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
 {
     //MOVEMENT
     private Rigidbody _rigidbody;
-    public float jumpForce = 200f;
+    
     public float speed = 0;
     private float movementX;
     private float movementY;
@@ -19,10 +19,10 @@ public class Player : MonoBehaviour
    
 
     private int platformIndex = 0;
-    
 
 
 
+    [SerializeField] public float jumpForce = 200f;
     [SerializeField] private TMP_Text _scoreText;
     [SerializeField] private GameObject _wallPrefab;
     [SerializeField] private GameObject _platformPrefab;
@@ -39,9 +39,8 @@ public class Player : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
 
         _scoreText.text = PlayerPrefs.GetString("Score");
+        ScoreValue = PlayerPrefs.GetInt("ScoreValue");
 
-        // méthode SceneManager.GetActiveScene stocké dans une variable
-        //_scene = SceneManager.GetActiveScene();
         // je récup tous mes éléments
         Debug.Log("La scène s'appelle: " + _scene.name + " et son index est : " + _scene.buildIndex);
     }
@@ -62,6 +61,11 @@ public class Player : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+    void OnJump(InputValue jumpValue)
+    { 
+        Debug.Log("je saute");
+        _rigidbody.AddForce(new Vector3(0f,jumpForce, 0f));
     }
 
     private void FixedUpdate()
@@ -109,10 +113,10 @@ public class Player : MonoBehaviour
     {
         //j'incrémente
         ScoreValue++;
-        // je crée un score en string 
+        // je modifie et récupère le score avec la key
         PlayerPrefs.SetString("Score", "Score : " + ScoreValue);
-
-        // je le récupère 
+        PlayerPrefs.GetInt("ScoreValue" + ScoreValue);
+        PlayerPrefs.SetInt("ScoreValue", ScoreValue);
         _scoreText.text = PlayerPrefs.GetString("Score");
 
         // si le score = 8 alors on change de scène ( niveau suivant )
@@ -121,10 +125,13 @@ public class Player : MonoBehaviour
             SceneManager.LoadScene("Level2");
             Debug.Log("Index de la scène active : " + _scene.name);
         }
-        // on récup le score pour le niveau suivant
-        //_scenario.Score = ScoreValue;
-        // on incrémente 
-        //SceneManager.LoadScene();
+    }
+    //fonction quand on quitte le jeu 
+    private void OnApplicationQuit()
+    {
+        // je supprime le score
+        PlayerPrefs.DeleteKey("ScoreValue");
+        PlayerPrefs.DeleteKey("Score");
 
     }
 
@@ -132,7 +139,7 @@ public class Player : MonoBehaviour
     private void OnDestroy()
     {
         //On supprime la clé une fois terminer.
-        PlayerPrefs.DeleteKey("Score");
+        //PlayerPrefs.DeleteKey("Score");
 
     }
 }
